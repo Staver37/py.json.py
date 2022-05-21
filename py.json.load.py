e1ebb1758fe31dcf608755b2c798cfc29e6274d9
Staver37 <102354895+Staver37@users.noreply.github.com>
2022-05-21 14:45:08 +0300

    Update py.json.load.py

--- py.json.load.py
+++ py.json.load.py
@@ -1,16 +1,6 @@
 # SAVE structure to ----> JSON file
 from json  import loads
 # data structure (Python)
-person = {
-    "name" : "John",
-    "age"  : 30,
-    "alive": True,
-    "interests":[
-        "programing",
-        "hacking",
-        "exe"
-    ]
-}
 
 # save the data
 file = open("person.json","r")
